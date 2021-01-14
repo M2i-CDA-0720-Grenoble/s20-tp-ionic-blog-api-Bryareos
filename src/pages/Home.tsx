d297711ed6993cb0a92dbec6406145c09bc34173
Bryareos <david-o@jhiki.fr>
2021-01-14 20:10:28 +0100

    1. Display all articles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import { Article, Layout } from '../components';
 import { IArticle } from '../models';
 import './Home.css';
 
 const Home: FC = () => {
-  // TODO - Récupérer la liste des articles dans le serveur
-  const articles: IArticle[] = [];
+    const API_BASEURL = 'http://localhost:3000'
+    const [articles, setArticles] = useState<IArticle[]>([])
+
+  useEffect(
+      () => {
+          fetch(`${API_BASEURL}/articles`)
+              .then(
+                  response => {
+                      if (!response.ok) {
+                          throw new Error('Error while fetching articles.')
+                      }
+                      return response.json()
+                  }
+              )
+              .then((json: IArticle[]) => {
+                  setArticles(json)
+              })
+      },
+      []
+  )
 
   return (
     <Layout title="Articles">
