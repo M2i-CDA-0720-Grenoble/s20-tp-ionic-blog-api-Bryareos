166d03791b19785e6c618e8b1d3411bf808e3935
Bryareos <david-o@jhiki.fr>
2021-01-14 20:14:35 +0100

    2. Display all categories

--- src/pages/AllCategories.tsx
+++ src/pages/AllCategories.tsx
@@ -1,10 +1,28 @@
-import React, { FC } from "react";
+import React, {FC, useEffect, useState} from "react";
 import { Category, Layout } from "../components";
-import { ICategory } from "../models";
+import {ICategory} from "../models";
 
 const AllCategories: FC = () => {
-  // TODO - Récupérer la liste des catégories dans le serveur
-  const categories: ICategory[] = [];
+    const API_BASEURL = 'http://localhost:3000'
+    const [categories, setCategories] = useState<ICategory[]>([])
+
+    useEffect(
+        () => {
+            fetch(`${API_BASEURL}/categories`)
+                .then(
+                    response => {
+                        if (!response.ok) {
+                            throw new Error('Error while fetching articles.')
+                        }
+                        return response.json()
+                    }
+                )
+                .then((json: ICategory[]) => {
+                    setCategories(json)
+                })
+        },
+        []
+    )
 
   return (
     <Layout title="Categories">
